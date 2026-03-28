b6bb596d7188987acfcd24a2d06d740bcf21bb59
Pragya Bamrara <21je0666@iitism.ac.in>
2026-03-29 00:52:18 +0530

    fix homepage login 2.0

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
-
+// ✅ FIRST: force login page
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/login.html");
 });
+
+// ✅ THEN static files
+app.use(express.static("public"));
 
 // Dummy DB
 const students = [
